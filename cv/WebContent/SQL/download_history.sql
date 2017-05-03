8ceb14bd4c0ec8abc307b89ef02d8e24fdd179cc
davide <davide@davide-SVE1512H1EW>
2017-05-03 20:48:59 +0200

    Fixed sql script

--- cv/WebContent/SQL/download_history.sql
+++ cv/WebContent/SQL/download_history.sql
@@ -1,8 +1,10 @@
 CREATE DATABASE `cv_database` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
+USE cv_database;
+
 CREATE TABLE `download_history` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `file_name` varchar(20) NOT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
